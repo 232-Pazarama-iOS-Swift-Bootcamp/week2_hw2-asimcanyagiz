681f8fb067d01a9e6d265d79018c487a3ba45f3e
Asim Can Yagiz <meycasim@gmail.com>
2022-10-02 16:58:06 +0300

    Initial Commit

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Asım can Yağız on 2.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
